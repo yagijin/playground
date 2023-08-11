88fc023385b0c1dc85bb6ce07fa684aedbccca46
yagijin <yagijin_dev@ymail.ne.jp>
2023-08-12 07:28:00 +0900

    update wasmを動くようにする

--- components/pages/WasmRuby/WasmRuby.tsx
+++ components/pages/WasmRuby/WasmRuby.tsx
@@ -1,8 +1,6 @@
 import { FC, useEffect, useState } from 'react'
-// @ts-ignore
-import { DefaultRubyVM } from 'ruby-head-wasm-wasi/dist/browser.umd'
-import styles from './WasmRuby.module.scss'
-
+import { Button, Heading, Text, Textarea, Flex, Center } from '@chakra-ui/react'
+import { AiFillCaretRight } from 'react-icons/ai'
 type props = {}
 
 let rubyVM: any
@@ -10,7 +8,24 @@ let rubyVM: any
 const WasmRuby: FC<props> = ({}) => {
   const [code, setCode] = useState('')
   const [execResult, setExecResult] = useState('')
+
+  const [isWasmLoaded, setIsWasmLoaded] = useState(false)
+
+  const [textAreaHeight, setTextAreaHeight] = useState(100)
+
   useEffect(() => {
+    setTextAreaHeight(window.innerHeight * 0.5)
+  }, [])
+
+  useEffect(() => {
+    // @ts-ignore
+    if (window?.['ruby-wasm-wasi'] === undefined) {
+      // wasm設定のためのjsが読み込まれるまでわざと無限ループさせる
+      setIsWasmLoaded(false)
+      return
+    }
+    // @ts-ignore
+    const { DefaultRubyVM } = window['ruby-wasm-wasi']
     ;(async () => {
       const response = await fetch(
         'https://cdn.jsdelivr.net/npm/ruby-head-wasm-wasi@latest/dist/ruby.wasm'
@@ -21,36 +36,70 @@ const WasmRuby: FC<props> = ({}) => {
 
       rubyVM.vm.printVersion()
     })()
-  }, [])
+  }, [isWasmLoaded])
+
   return (
-    <div id="wasm-container" className={styles['wasm-container']}>
-      <h1>Rubyが動くよ。</h1>
-      <p>
-        左のテキストエリアにコードを入力して実行を押してね。実行結果が左に表示されるよ。
-      </p>
-      <div className={styles.container}>
-        <div className={styles['textarea-container']}>
-          <textarea
-            className={styles.textarea}
+    <>
+      <script
+        src="https://cdn.jsdelivr.net/npm/@ruby/wasm-wasi@latest/dist/browser.umd.js"
+        async
+      ></script>
+      <div
+        id="wasm-container"
+        style={{
+          margin: '2rem',
+          display: 'flex',
+          flexDirection: 'column',
+          gap: '1rem',
+        }}
+      >
+        <Heading>Wasm Ruby</Heading>
+        <Text>
+          左のテキストエリアにコードを入力して実行を押すと実行結果が左に表示される。
+          <br />例 : 1 + 1
+        </Text>
+
+        <Flex align="center" height={textAreaHeight}>
+          <Textarea
+            flex={5}
             onChange={(e) => {
               setCode(e.target.value)
             }}
-          ></textarea>
-          <textarea value={execResult} className={styles.textarea}></textarea>
-        </div>
-        <button
-          onClick={() => {
-            try {
-              setExecResult(rubyVM.vm.eval(code).toString())
-            } catch (err: any) {
-              setExecResult(err)
-            }
-          }}
-        >
-          実行
-        </button>
+            placeholder="Ruby Code Here!!"
+            size="sm"
+            height="100%"
+            borderRadius="1rem"
+            resize={'none'}
+          ></Textarea>
+          <AiFillCaretRight style={{ flex: 1 }} />
+          <Text
+            flex={5}
+            backgroundColor="lightgray"
+            width="100%"
+            height="100%"
+            padding="1rem"
+            borderRadius="1rem"
+          >
+            {execResult}
+          </Text>
+        </Flex>
+        <Center>
+          <Button
+            width={'4rem'}
+            colorScheme="teal"
+            onClick={() => {
+              try {
+                setExecResult(rubyVM.vm.eval(code).toString())
+              } catch (err: any) {
+                setExecResult(err)
+              }
+            }}
+          >
+            実行
+          </Button>
+        </Center>
       </div>
-    </div>
+    </>
   )
 }
 
